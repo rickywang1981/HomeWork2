b169cf397df3d6fa63d8a3fada5d084f1760a9cf
rickywang1981 <s9213037@gmail.com>
2015-09-23 19:03:24 +0800

    MyHomework2

---
+++ PotterShoppingCart.Tests/UnitTest1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestClass]
+    public class PotterShoppingCartTests
+    {
+        private List<Book> books;
+
+        [TestInitialize()]
+        public void TestInit()
+        {
+            books = new List<Book>();
+        }
+
+        [TestMethod()]
+        public void buy_volumn_one_equal_100()
+        {
+            books.Add(new Book { Name = "HarryPotter Volumn 1", Price = 100 });
+
+            double expected = 100;
+
+            double sum = ShoppingCart.CalcPrice(books);
+
+            Assert.AreEqual(expected, sum);
+        }
+
+        [TestMethod()]
+        public void buy_volumn_one_and_two_equal_190()
+        {
+            books.Add(new Book {Name = "HarryPotter Volumn 1", Price = 100});
+            books.Add(new Book {Name = "HarryPotter Volumn 2", Price = 100});
+
+            double expected = 190;
+
+            double sum = ShoppingCart.CalcPrice(books);
+
+            Assert.AreEqual(expected, sum);
+        }
+
+        [TestMethod()]
+        public void buy_volumn_one_two_three_four_five_equal_375()
+        {
+            books.Add(new Book { Name = "HarryPotter Volumn 1", Price = 100 });
+            books.Add(new Book { Name = "HarryPotter Volumn 2", Price = 100 });
+            books.Add(new Book { Name = "HarryPotter Volumn 3", Price = 100 });
+            books.Add(new Book { Name = "HarryPotter Volumn 4", Price = 100 });
+            books.Add(new Book { Name = "HarryPotter Volumn 5", Price = 100 });
+
+            double expected = 375;
+
+            double sum = ShoppingCart.CalcPrice(books);
+
+            Assert.AreEqual(expected, sum);
+        }
+
+        [TestMethod()]
+        public void buy_volumn_one_two_three_three_equal_370()
+        {
+            books.Add(new Book { Name = "HarryPotter Volumn 1", Price = 100 });
+            books.Add(new Book { Name = "HarryPotter Volumn 2", Price = 100 });
+            books.Add(new Book { Name = "HarryPotter Volumn 3", Price = 100 });
+            books.Add(new Book { Name = "HarryPotter Volumn 3", Price = 100 });
+
+            double expected = 370;
+
+            double sum = ShoppingCart.CalcPrice(books);
+
+            Assert.AreEqual(expected, sum);
+        }
+
+        [TestMethod()]
+        public void buy_volumn_one_two_two_three_three_equal_460()
+        {
+            books.Add(new Book { Name = "HarryPotter Volumn 1", Price = 100 });
+            books.Add(new Book { Name = "HarryPotter Volumn 2", Price = 100 });
+            books.Add(new Book { Name = "HarryPotter Volumn 2", Price = 100 });
+            books.Add(new Book { Name = "HarryPotter Volumn 3", Price = 100 });
+            books.Add(new Book { Name = "HarryPotter Volumn 3", Price = 100 });
+
+            double expected = 460;
+
+            double sum = ShoppingCart.CalcPrice(books);
+
+            Assert.AreEqual(expected, sum);
+        }
+    }
+}
---
+++ PotterShoppingCart/ShoppingCart.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotterShoppingCart
+{
+    public class ShoppingCart
+    {
+        static Dictionary<int, double> discounts = new Dictionary<int, double>()
+        {
+            {1, 1.0},
+            {2, 0.95},
+            {3, 0.9},
+            {4, 0.8},
+            {5, 0.75}
+        }; 
+
+        public static double CalcPrice(List<Book> books)
+        {
+            // determine book category by book name
+            var bookCategory = books.GroupBy(x => x.Name);
+
+            // determine how many books in each category
+            var categoryCountGroup = bookCategory.Select(x => x.Count())
+                                                 .Distinct()
+                                                 .OrderBy(c => c).ToList();
+
+            double sum = 0;
+            int prevMinCount = 0;
+            foreach (var c in categoryCountGroup)
+            {
+                double partialSum = 0;
+                int discountKey = 0;
+                foreach (var bc in bookCategory)
+                {
+                    if (bc.Count() >= c)
+                    {
+                        // get a book in the group
+                        var k = bc.ElementAt(0);
+
+                        // sum the book price in the group
+                        partialSum += k.Price * (c - prevMinCount);
+
+                        // increment discount key
+                        discountKey++;
+                    }
+                }
+
+                partialSum *= discounts[discountKey];                
+
+                prevMinCount = c;
+
+                // result
+                sum += partialSum;
+            }
+
+            return sum;
+        }
+    }
+}
